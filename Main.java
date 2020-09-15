64a99bcb8237552f0d834a62ea2e66f5ecfd73e6
tomad6909 <tomad6909@wrdsb.ca>
2020-09-15 15:41:48 +0000

    Finished A2Q3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program determines the correct order for 2 numbers
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user
+    Scanner input = new Scanner(System.in);
+
+    // ask user for the first integer
+    System.out.println("Please enter an integer:");
+    int integer1 = input.nextInt();
+
+    // ask user for the second integer
+    System.out.println("Please enter another integer:");
+    int integer2 = input.nextInt();
+
+    // determine which integer is bigger
+    if(integer1 > integer2){
+      System.out.println("Your numbers in ascending order are " + integer2 + "," + integer1);
+    } else if(integer1 == integer2){
+      System.out.println("Your numbers in ascending order are " + integer2 + "," + integer1);
+    } else {
+      System.out.println("Your numbers in ascending order are " + integer1 + "," + integer2);
+    }
     
   }
 }
